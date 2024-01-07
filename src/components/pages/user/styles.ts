6ac411a87bd9184db2266bf51165f3bfbab8c476
mariahnoren <maria@lunacreatives.se>
2024-01-07 20:46:07 +0100

    refactor: error alert position

--- src/components/pages/user/styles.ts
+++ src/components/pages/user/styles.ts
@@ -1,6 +1,7 @@
 import { Grid, styled } from '@mui/material';
 
 export const ErrorContainer = styled(Grid)(() => ({
+  flex: 1,
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
